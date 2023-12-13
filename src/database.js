e8cb899b08c0c9c815b8d91f1a603665400eecbe
Raj Karmakar <raj@ymirlabs.com>
2023-12-13 22:52:11 +0530

    Created in-memory data store

---
+++ src/database.js
@@ -0,0 +1,44 @@
+exports.inventory = [
+	{
+		id: 1,
+		name: 'item1',
+		price: '300 INR',
+	},
+	{
+		id: 2,
+		name: 'item2',
+		price: '400 INR',
+	},
+	{
+		id: 3,
+		name: 'item3',
+		price: '4300 INR',
+	},
+	{
+		id: 4,
+		name: 'item4',
+		price: '3300 INR',
+	},
+	{
+		id: 5,
+		name: 'item5',
+		price: '5300 INR',
+	},
+	{
+		id: 6,
+		name: 'item6',
+		price: '3006 INR',
+	},
+	{
+		id: 7,
+		name: 'item7',
+		price: '30032 INR',
+	},
+	{
+		id: 8,
+		name: 'item8',
+		price: '800 INR',
+	}
+];
+
+exports.cart = [];
