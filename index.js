b0dfd6bcb5abc1da39c8df3bd740317a3ca00ec5
Raj Karmakar <raj@ymirlabs.com>
2023-12-15 10:53:46 +0530

    Add comments

--- index.js
+++ index.js
@@ -12,35 +12,39 @@ const PORT = 3001 || process.env.PORT;
 
 app.use(express.json());
 
-// Admin APIs
+// Server health checkup
 app.get(adminRoutes.SERVER_HEALTH_CHECKUP, (req, res) => {
 	res.send('<h1>Server is running properly</h1>');
 });
 
+// Admin API to generate Discount code
 app.post(
 	adminRoutes.GENERATE_DISCOUNT_CODE,
 	validate(generateDiscountCodeValidator),
 	generateDiscountCodeController
 );
 
+// Admin API to generate Store summary
 app.get(
 	adminRoutes.GENERATE_STORE_SUMMARY,
 	generateStoreSummaryController
 );
 
-// Client APIs
+// Client API to add items to Cart
 app.post(
 	clientRoutes.ADD_TO_CART,
 	validate(addToCartValidator),
 	addToCartController
 );
 
+// Client API for checking out
 app.post(
 	clientRoutes.CHECKOUT,
 	validate(checkoutValidator),
 	checkoutController
 );
 
+// Catch Validation error
 app.use(function(err, req, res, next) {
 	if (err instanceof ValidationError) {
 		return res.status(err.statusCode).json(err)
